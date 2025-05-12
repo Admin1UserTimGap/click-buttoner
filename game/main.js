8ee8bc9a8a7be2cb9a4a98872786c7bd869e98a4
Admin1UserTimGap <148370595+Admin1UserTimGap@users.noreply.github.com>
2025-05-12 23:21:41 +0300

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,1 +1,16 @@
+Clicks=0;
 
+const ClickButton=document.getElementById("btn-main")
+
+function displayUpdate(clicks) {
+  display=document.getElementById("display")
+  display.innerHTML=clicks;
+}
+
+function onClick() {
+  Clicks++;
+  displayUpdate(Clicks);
+}
+
+ClickButton.addEventListener('click', onClick)
+displayUpdate()
