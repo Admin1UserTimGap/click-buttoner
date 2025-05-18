00ee8de84f1fe8dc0581250914761963996da7b3
Admin1UserTimGap <148370595+Admin1UserTimGap@users.noreply.github.com>
2025-05-18 10:37:06 +0300

    Update main.js

--- game/main.js
+++ game/main.js
@@ -3,11 +3,12 @@ let Clicks=0;
 function displayUpdate(clicks) {
   $('display').text(clicks)
 }
-
 function onClick() {
   Clicks++;
+  console.log("Code Execution Line 8")
   displayUpdate(Clicks);
 }
-
-$('#btn-main').click(onClick); 
-displayUpdate(Clicks);
+$(document).ready(function() {
+  $('#btn-main').click(onClick); 
+  displayUpdate(Clicks);
+})
