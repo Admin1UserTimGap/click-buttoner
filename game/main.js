38c639ff4689f3c90eec4cb914dbd9828778dbbf
Admin1UserTimGap <148370595+Admin1UserTimGap@users.noreply.github.com>
2025-05-18 10:45:56 +0300

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,18 +1,15 @@
 let Clicks=0;
-console.log("TEST: BOOTUP...")
 
 function displayUpdate(clicks) {
+  console.log("The problem is in line 5!")
   $('display').text(clicks)
 }
 function onClick() {
   Clicks++;
-  console.log("Code Execution Line 8")
   displayUpdate(Clicks);
 }
 $(document).ready(function() {
-  console.log("Code Execution Line 12")
   $('#btn-main').on('click', function() {
-    console.log("Code Execution Line 14")
     onClick()
   });
   displayUpdate(Clicks);
